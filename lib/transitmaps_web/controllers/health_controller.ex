523589a2f1db168e7b707b89c63c4e581093a599
Michael Richards <56988314+LaggySon@users.noreply.github.com>
2026-07-21 14:28:18 -0400

    Expose deployed Railway commit

--- lib/transitmaps_web/controllers/health_controller.ex
+++ lib/transitmaps_web/controllers/health_controller.ex
@@ -2,6 +2,9 @@ defmodule TransitmapsWeb.HealthController do
   use TransitmapsWeb, :controller
 
   def show(conn, _params) do
-    json(conn, %{status: "ok"})
+    json(conn, %{
+      status: "ok",
+      commit: System.get_env("RAILWAY_GIT_COMMIT_SHA")
+    })
   end
 end
